3f9c535b82e918a380eeaa5d45ee1a071fe349c6
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-11-13 19:05:19 -0500

    base primer sprint

---
+++ fontend_monitorias/src/Components/BoxSubject.js
@@ -0,0 +1,12 @@
+// src/Components/BoxSubject.js
+import React from 'react';
+
+const BoxSubject = () => {
+  return (
+    <div>
+      <p>Caja de las materias</p>
+    </div>
+  );
+}
+
+export default BoxSubject;
---
+++ fontend_monitorias/src/Components/Header.js
@@ -0,0 +1,12 @@
+// src/Components/Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Bienvenidos a Mi Aplicación</h1>
+    </div>
+  );
+}
+
+export default Header;
