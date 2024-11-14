6e865d5ab0088e8099257f8a772762b00b333717
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-11-13 19:05:19 -0500

    base primer sprint

---
+++ fontend_monitorias/src/Components/Auth.js
@@ -0,0 +1,12 @@
+// src/Components/Auth.js
+import React from 'react';
+
+const Auth = () => {
+  return (
+    <div>
+      <p>Autenticación en la App</p>
+    </div>
+  );
+}
+
+export default Auth;
