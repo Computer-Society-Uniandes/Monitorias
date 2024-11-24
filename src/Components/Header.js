7b09cd23f8e6e22d21c5369b931d056047409a9b
Manu_Love <107737902+mloveral@users.noreply.github.com>
2024-11-24 18:02:20 -0500

    Avances header campos

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,19 @@
 // src/Components/Header.js
 import React from 'react';
+import'../Style/Header.css'
 
 const Header = () => {
   return (
-    <div>
-      <h1>Bienvenidos a Mi Aplicación</h1>
-    </div>
+    <header className='header'>
+      <a href = "/" className='logo'>MonitorPro</a>
+      <nav className='navbar'>
+        <a href = "/">Inicio</a>
+        <a href = "/">Explorar Materias</a>
+        <a href = "/">Buscar Tutores</a>
+        <a href = "/">Acerca de</a>
+      </nav>
+      
+    </header>
   );
 }
 
