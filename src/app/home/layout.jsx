10151e3e5d259a2e80097ac600e1d0c1351784da
Feru_34 <f.rueda4@uniandes.edu.co>
2025-06-22 19:56:14 -0500

    Menos Tailwind más orden

--- src/app/home/layout.jsx
+++ src/app/home/layout.jsx
@@ -4,6 +4,10 @@ import "../globals.css";
 export default function Layout({children}) {
     return(
         < >
+        <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+            rel="stylesheet"
+            />
             <Header suppressHydrationWarning/>
             {children}
         </>
