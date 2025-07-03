c5c1cbb042098d4f9e4b2df34a898561b12a85b7
Maria Lu <m.benavidesd@uniandes.edu.co>
2025-07-03 14:07:31 -0500

    agregar un formato bonito para home, con navbar raro

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,13 @@ const routes = {
     EXPLORE:"/home/explore",
     LOGIN:"/auth/login",
     REGISTER:"/auth/register",
-    PROFILE: "/home/profile"
+    PROFILE: "/home/profile",
+    
+    // Rutas específicas para tutores
+    TUTOR_INICIO: "/tutor/inicio",
+    TUTOR_MIS_TUTORIAS: "/tutor/mis-tutorias",
+    TUTOR_MATERIAS: "/tutor/materias", 
+    TUTOR_PAGOS: "/tutor/pagos"
 };
 
 export default routes;
